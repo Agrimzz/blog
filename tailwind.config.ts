3b4f8b4bdff6512ef2b718f43e31cb1b25980af1
Agrim Pradhananga <agrimp91@gmail.com>
2025-02-20 13:25:28 +0545

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#2cd6c9",
+        background: "#141414",
       },
     },
   },
